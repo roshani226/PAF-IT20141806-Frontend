83571891ed29ed8fae3ee7d44aacfa5cee3b87f5
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-15 14:06:47 +0530

    unitService.java
    unitService.java

---
+++ UnitManage/src/com/UnitService.java
@@ -0,0 +1,13 @@
+package com;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML 
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+import model.Unit;
+
