4d73279fed4dbf796937cceeb5d90adfc97111e3
roshani226 <86221628+roshani226@users.noreply.github.com>
2022-05-15 14:08:03 +0530

    Update UnitService.java

--- UnitManage/src/com/UnitService.java
+++ UnitManage/src/com/UnitService.java
@@ -35,3 +35,28 @@ public class UnitService {
 	 String output = UnitObj.insertUnit (user_account_no, usage_date, used_units, price_per_unit);
 	return output;
 	}
+
+		@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateUnit(@FormParam("unit_id") String unit_id,@FormParam("user_account_no") String user_account_no,			
+			 @FormParam("usage_date") String usage_date,
+			 @FormParam("used_units") String used_units,
+			 @FormParam("price_per_unit") String price_per_unit)
+	{
+	
+	 String output = UnitObj.updateUnit(unit_id, user_account_no, usage_date, used_units, price_per_unit);
+	return output;
+	} 
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteUnit(@FormParam("id") String id)
+	{
+	
+	 String output = UnitObj.deleteUnit(id);
+	return output;
+	}
